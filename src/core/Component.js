4b106a140aa3d3577191f334fe16b2d4991f8212
정승덕 <akk9132@naver.com>
2023-06-11 20:59:03 +0900

    feat: Add core component class

---
+++ src/core/Component.js
@@ -0,0 +1,32 @@
+class Component {
+  constructor(target, props = {}) {
+    this.target = target;
+    this.props = props;
+    this.setup();
+    this.render();
+    this.mounted();
+  }
+  setup() {}
+  template() {
+    return "";
+  }
+  render() {
+    this.target.innerHTML = this.template();
+  }
+  mounted() {}
+  setState(newState) {
+    let isRerender = false;
+    for (const key in Object.keys(newState)) {
+      if (this.state[key] === newState[key]) continue;
+      isRerender = true;
+    }
+
+    if (!isRerender) return;
+
+    this.state = { ...this.state, newState };
+    this.render();
+    this.mounted();
+    this.updated();
+  }
+  updated() {}
+}
