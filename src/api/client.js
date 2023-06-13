3190583eca995ab27a75cf886b9ec9021aaa2c33
정승덕 <akk9132@naver.com>
2023-06-13 21:37:11 +0900

    feat: Add memory cache

--- src/api/client.js
+++ src/api/client.js
@@ -1,13 +1,21 @@
 const baseURL = "http://localhost:3000/api";
 
+const cache = {};
+
 const client = {
   get: async (path) => {
     try {
-      const response = await fetch(`${baseURL}${path}`, {
+      const url = `${baseURL}${path}`;
+      if (cache[url]) {
+        return cache[url];
+      }
+
+      const response = await fetch(url, {
         method: "GET",
       });
 
       const data = await response.json();
+      cache[url] = data;
 
       return data;
     } catch (err) {
